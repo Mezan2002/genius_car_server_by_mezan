fd6eefad7ed7425807a884ce4bcce364df62b7b5
Mezan2002 <mezanurr009@gmail.com>
2022-11-02 03:41:03 +0600

    Service data and single service data loaded

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-// middle wares
+// middle wares start
 app.use(cors());
 app.use(express.json());
+// middle wares end
 
-// mongo DB connection
+// mongo DB connection start
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// mongo DB connection end
 
-async function app() {
+async function run() {
   try {
     const serviceCollection = client
       .db("geniusCarByMezan")
-      .collection("service");
+      .collection("services");
+
+    // get all services start
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    // get all services end
+
+    // get one service start
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    // get one service end
   } finally {
   }
 }
 
-app().catch((error) => console.log(error));
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Genius Car Server is Running");
